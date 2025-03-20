37435b3d278cbd902001fc24ce630454fe214bca
Victor Gabriel <victorgabriel08fs@gmail.com>
2025-03-20 00:16:33 -0300

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
-export default {
+module.exports = {
   apps: [
     {
       name: "treino-tracker",
-      script: "src/index.js",
+      script: "./src/index.js", // Certifique-se de que o caminho está correto
       watch: true,
       ignore_watch: ["src/database/dev.db"],
     },
