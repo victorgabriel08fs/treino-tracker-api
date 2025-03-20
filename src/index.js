8de3ce8d9e4e8b1b1e9033ab950e0d48fb0d164b
Victor Gabriel <victorgabriel08fs@gmail.com>
2025-03-19 23:07:46 -0300

    hello world

--- src/index.js
+++ src/index.js
@@ -8,5 +8,7 @@ app.use(express.json());
 
 app.use('/api', routes);
 
+app.use("/", (req, res) => res.json({ message: "Hello World" }));
+
 app.listen(3000, () => console.log('Server running on port 3000'));
 
