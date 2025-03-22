944ba05f3483eac054fc0a2ea80ef0dd54755eed
Victor Gabriel <victorgabriel08fs@gmail.com>
2025-03-22 11:32:28 -0300

    certify

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import 'dotenv/config'
+import https from 'https';
+import fs from 'fs';
 import cors from 'cors';
 import routes from './routes/index.routes.js';
 import { errorMiddleware } from './middlewares/ErrorMiddleware.js';
@@ -9,7 +11,22 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const PORT = process.env.PORT || 3000
+
+if(process.env.APP_ENV){
+    // Carregar os certificados SSL
+const options = {
+    key: fs.readFileSync("/etc/letsencrypt/live/treinamente-api.duckdns.org/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/treinamente-api.duckdns.org/fullchain.pem"),
+  };
+  
+  // Criar o servidor HTTPS
+  https.createServer(options, app).listen(PORT, () => {
+    console.log(`Servidor rodando em https://treinamente-api.duckdns.org:${PORT}`);
+  });
+}
+
 app.use('/api', errorMiddleware.handle, routes);
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
